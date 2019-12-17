c089e0e22bcb9833952d1f4c993cf62770dbb495
Wichard Riezebos <wichardriezebos@gmail.com>
2019-12-17 22:43:53 +0100

    added index file

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { Just } from './limonad/just';
+export { Limonad } from './limonad/limonad';
+export { Nothing } from './limonad/nothing';
+export { Schematics } from './limonad/schematics';
